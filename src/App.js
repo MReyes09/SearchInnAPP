f9e9f3e255d9c489769c7122cd6aafa568571cf5
Matthew Reyes <mreyes-20161552@kinal.edu.gt>
2021-09-17 17:16:40 -0600

    mostrar pdf

--- src/App.js
+++ src/App.js
@@ -2,31 +2,18 @@ import './App.css';
 import React, {Fragment, useState} from 'react';
 // import { RestSearch } from './API/dynamoDB';
 import axios from "axios";
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 
 var baseUri = "https://yz8l6z59zh.execute-api.us-east-1.amazonaws.com/prod?";//path de dynamo DB
 var foundSearch;
+var urlSearch;
 
-function App() {
-  
-  return (
-    <div className="App">
-      <div className="App-header">
-        <div className="buscadorCuadro">
-          <h1 className="h1-Title">Ingresa Datos</h1>
-          <Buscador />
-          <hr/>
-          <p className="parraf">Resultado...</p>          
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const Buscador = () =>{
+function App (){
 
   const [datoSearch, setDatos] = useState({
     tienda: '',
     referencia: '',
+    abierto: false,
   })
 
   const handleInputChange = (event) => {
@@ -34,8 +21,6 @@ const Buscador = () =>{
       ...datoSearch,
       [event.target.name] : event.target.value,
     })
-    
-    console.log(datoSearch)
   }
 
   const buscarDatos_axios = (event) => {
@@ -43,61 +28,70 @@ const Buscador = () =>{
 
     axios.get(baseUri + 'tienda=' + datoSearch.tienda + '&referencia=' + datoSearch.referencia)
     .then(result => {
-       
+
       let expReg = new RegExp(".pdf" || "files" || "pdf", "ig");
       let expReg2 = new RegExp("report" || "documento" || "document", "ig");
-
+    
       if(expReg.test(result.data.url) != true){
         if(expReg2.test(result.data.url) != true){
           console.log('no hay conincidencias')
         }else if(expReg2.test(result.data.url) === true){
           foundSearch = result;
-          console.log('concidencia en la url es un pdf opcion 2 ', foundSearch);
-          ConditionalView(true);
+          urlSearch = result.data.url;
+          setDatos({
+            ...datoSearch,
+            abierto: !datoSearch.abierto
+          })
+          console.log(foundSearch);
         }
       }else if(expReg.test(result.data.url) === true){
         foundSearch = result;
-        console.log('concidencia en la url es un pdf opcion 1 ', foundSearch);
-        ConditionalView(true);
+        urlSearch = result.data.url;
+        setDatos({
+          ...datoSearch,
+          abierto: !datoSearch.abierto
+        })
+        console.log(foundSearch);
       }
     }).catch(console.log());
   }
-  
+
   return (
-    <Fragment>
-      <form onSubmit={buscarDatos_axios}>
+    <div className="Body">
+      <div className="buscadorCuadro">
+        <h1 className="h1-Title">Ingresa Datos</h1>
 
-        <div className="form-container">
-          <div className="form-group">
-            <input className="form-control" type="text" name="tienda" placeholder="tienda" onChange={handleInputChange}/>
+        <form onSubmit={buscarDatos_axios}>
+
+          <div className="form-container">
+            <div className="form-group">
+              <input className="form-control" type="text" name="tienda" placeholder="tienda" onChange={handleInputChange}/>
+            </div>
+
+            <div className="form-group">
+              <input className="form-control" type="text" name="referencia" placeholder="Referencia" onChange={handleInputChange}/>
+            </div>
           </div>
 
-          <div className="form-group">
-            <input className="form-control" type="text" name="referencia" placeholder="Referencia" onChange={handleInputChange}/>
-          </div>
-        </div>
+          <button className="btn-Buscar" type="submit">Buscar</button>
 
-        <button className="btn-Buscar" type="submit">Buscar</button>
+        </form>
 
-      </form>
-    </Fragment>
-  )
-}
-
-function ConditionalView (condicion) {
-  
-  if(condicion != null || undefined){
-    return (
-      <App />
-    )
-  }else if(condicion === true){
-    return (
-      <div>
-        <h1>Se obtuvo una url valida</h1>
-        <button className="btn-Buscar" type="submit" >Volver</button>
+        <hr/>
+        <p className="parraf">Resultado...</p>          
       </div>
-    )
-  }
+      
+      <Modal className="Modal" isOpen={datoSearch.abierto}>
+        <ModalHeader>
+          <h1>Resultados de la busqueda</h1>
+        </ModalHeader>
+        <ModalBody>
+          <object type="application/pdf" data={urlSearch}> </object>
+        </ModalBody>
+      </Modal>
+    </div>
+  )
+  
 }
 
-export default ConditionalView;
+export default App;
